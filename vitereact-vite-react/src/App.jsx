86cd87ad99751fb2752caf2ea99a14bad062f78a
Manikanta Sriram <manikantasriram98@gmail.com>
2022-03-14 12:19:46 +0530

    All Changes new

--- vitereact-vite-react/src/App.jsx
+++ vitereact-vite-react/src/App.jsx
@@ -11,37 +11,37 @@ function App() {
           <Card 
           Img="https://www.pinkvilla.com/files/styles/amp_metadata_content_image_min_696px_wide/public/suriya_latest_poster_mian_0.jpg?itok=EdriMQxO"
           Title = "ETHARKKUM THUNINDHAVAN"
-          Content = "All about Etharkkum Thunindhavan"
+          Content = "WATCH IT HERE"
           Link = "https://www.imdb.com/title/tt10340562/"
           />
           <Card
           Img="https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg"
           Title = "JAI BHIM"
-          Content = "All about Jai Bhim"
+          Content = "WATCH IT HERE"
           Link = "https://www.imdb.com/title/tt15097216/?ref_=fn_al_tt_1"
           />
           <Card
           Img="https://i.pinimg.com/originals/41/99/00/4199001a1cced80374c89ccf1078d345.jpg"
           Title = "SOORARAI POTTRU"
-          Content = "All about Soorarai Pottru"
+          Content = "WATCH IT HERE"
           Link = "https://www.imdb.com/title/tt10189514/?ref_=nv_sr_srsg_0  "
           />
           <Card
           Img="https://wallpapercave.com/wp/wp4669929.jpg"
           Title = "KAAPPAAN"
-          Content = "All about Kaappaan"
+          Content = "WATCH IT HERE"
           Link = "https://www.imdb.com/title/tt8411144/?ref_=fn_al_tt_3"
           />
           <Card
           Img="https://moviegalleri.net/wp-content/gallery/ngk-movie-release-posters/suriya-ngk-movie-release-posters-2b5ac77.jpg"
           Title = "NGK  "
-          Content = "All about NGK"
+          Content = "WATCH IT HERE"
           Link = "https://www.imdb.com/title/tt8094252/?ref_=fn_al_tt_1"
           />
           <Card
-          Img="https://media-cache.cinematerial.com/p/500x/n9fgc9uv/thaanaa-serndha-koottam-indian-movie-poster.jpg?v=1514888808"
+          Img="https://images.fandango.com/ImageRenderer/820/0/redesign/static/img/default_poster.png/0/images/masterrepository/fandango/208631/thaanaa-Tamil-Theater.jpg"
           Title = "THAANAA SERNDHA KOOTTAM"
-          Content = "All about Thaanaa Serndha Koottam"
+          Content = "WATCH IT HERE"
           Link = "https://www.imdb.com/title/tt6237966/?ref_=nv_sr_srsg_0"
           />
     </div>
