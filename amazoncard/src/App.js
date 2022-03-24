7cf191c88880fc37df62929111ed0d7aaaa1f01a
Manikanta Sriram <manikantasriram98@gmail.com>
2022-03-24 15:07:17 +0530

    Changed

---
+++ amazoncard/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Card from './Components/Card';
+import './Components/Card.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
